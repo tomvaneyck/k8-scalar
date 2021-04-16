d3ebdce4ae2d141f240eda4199d29fe43a00a21d
Tom Van Eyck <tom.vaneyck@telenet.be>
2021-04-16 11:04:15 +0200

    Changed to local repo

--- practicum/scalar/src/be/kuleuven/distrinet/scalar/requests/SaasRequest.java
+++ practicum/scalar/src/be/kuleuven/distrinet/scalar/requests/SaasRequest.java
@@ -2,19 +2,15 @@ package be.kuleuven.distrinet.scalar.requests;
 
 import be.kuleuven.distrinet.scalar.core.User;
 import be.kuleuven.distrinet.scalar.exceptions.RequestException;
-import be.kuleuven.distrinet.scalar.users.SaaSUser;
-import java.time.LocalDateTime;
-import java.util.Set;
 
-import java.io.*;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-public class SaaSRequest extends Request {
-    public SaaSRequest(User usr) {
+public class SaasRequest extends Request {
+    public SaasRequest(User usr) {
         super(usr, true);
     }
 
